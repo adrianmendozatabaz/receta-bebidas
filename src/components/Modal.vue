d31831820ac9ed15dba002158c5cf4b6b65deec6
adrianmendozatabaz <myamyamya29@gmail.com>
2026-01-29 16:18:22 -0600

    feat: end class 243 add function to button favoritos

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -3,9 +3,11 @@ import { Dialog, DialogPanel, DialogTitle, TransitionChild, TransitionRoot } fro
 
 import { useModalStore } from '@/stores/modal';
 import { useBebidasStore } from '@/stores/bebidas';
+import { useFavoritosStore } from '@/stores/favoritos';
 
 const modal = useModalStore();
 const bebidas = useBebidasStore();
+const favoritos = useFavoritosStore();
 
 const formatearIngredientes = () => {
     const ingredientesDiv = document.createElement('div');
@@ -68,6 +70,10 @@ const formatearIngredientes = () => {
                                 <button type="button"
                                     class="w-full rounded bg-gray-600 p-3 font-bold uppercase text-white shadow hover:bg-gray-500"
                                     @click="modal.handleClicModal">Cerrar</button>
+
+                                <button type="button"
+                                    class="w-full rounded bg-orange-600 p-3 font-bold uppercase text-white shadow hover:bg-orange-500"
+                                    @click="favoritos.handleClicFavoritos">Agregar a favoritos</button>
                             </div>
                         </DialogPanel>
                     </TransitionChild>
