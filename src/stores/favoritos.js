3a8847cbb09c5354eeaa0cb56fe9987bec4dd12e
adrianmendozatabaz <myamyamya29@gmail.com>
2026-01-29 16:35:22 -0600

    feat: end class 246 get favoritos from LocalStorage

--- src/stores/favoritos.js
+++ src/stores/favoritos.js
@@ -7,6 +7,10 @@ export const useFavoritosStore = defineStore('favoritos', () => {
     const bebidas = useBebidasStore();
     const favoritos = ref([]);
 
+    onMounted(() => {
+        favoritos.value = JSON.parse(localStorage.getItem('favoritos')) ?? [];
+    })
+
     watch(favoritos, () => {
         sincronizarLocalStorage();
     }, {
