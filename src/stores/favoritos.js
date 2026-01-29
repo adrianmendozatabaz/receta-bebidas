2ea7da438297679e66da76747daabbaf22529a91
adrianmendozatabaz <myamyamya29@gmail.com>
2026-01-29 16:24:19 -0600

    feat: end class 244 add favoritos to ref

--- src/stores/favoritos.js
+++ src/stores/favoritos.js
@@ -1,12 +1,18 @@
 import { defineStore } from "pinia";
+import { useBebidasStore } from "./bebidas";
+import { ref } from "vue";
 
 export const useFavoritosStore = defineStore('favoritos', () => {
 
+    const bebidas = useBebidasStore();
+    const favoritos = ref([]);
+
     const handleClicFavoritos = () => {
-        console.log('add');
+        favoritos.value.push(bebidas.receta);
     }
 
     return {
+        favoritos,
         handleClicFavoritos
     }
 })
