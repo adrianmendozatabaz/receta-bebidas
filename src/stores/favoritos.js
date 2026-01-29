98d16484a83c001ae3913cdb5237d0b6e98174f8
adrianmendozatabaz <myamyamya29@gmail.com>
2026-01-29 16:58:34 -0600

    feat: end class 248 export functions

--- src/stores/favoritos.js
+++ src/stores/favoritos.js
@@ -36,6 +36,7 @@ export const useFavoritosStore = defineStore('favoritos', () => {
 
     return {
         favoritos,
-        handleClicFavoritos
+        handleClicFavoritos,
+        existeFavorito
     }
 })
