3e38145d297da4fbaf8cddad0aeb042b32d755c6
adrianmendozatabaz <myamyamya29@gmail.com>
2026-02-02 16:48:43 -0600

    feat: end class 251 add text when are any favoritos

--- src/views/FavoritosView.vue
+++ src/views/FavoritosView.vue
@@ -7,6 +7,9 @@ const favoritos = useFavoritosStore();
 
 <template>
     <h1 class="text-6xl font-extrabold">Favoritos</h1>
+
+    <p v-if="favoritos.noFavoritos" class="text-center text-2xl mt-10">No hay Favoritos.</p>
+
     <div class="grid grid-cols-1 md:grid-cols-2 2xl:grid-cols-3 my-10 gap-10">
         <Receta v-for="receta in favoritos.favoritos" :receta="receta" :key="receta.idDrink" />
     </div>
