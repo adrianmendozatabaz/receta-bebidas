7b8d91ccebdaa4216bb186de174b05d915c05d98
adrianmendozatabaz <myamyamya29@gmail.com>
2026-02-04 16:58:53 -0600

    feat: end class 256 add watch when a notification is send

--- src/stores/notificacion.js
+++ src/stores/notificacion.js
@@ -1,4 +1,4 @@
-import { ref } from "vue";
+import { ref, watch } from "vue";
 import { defineStore } from "pinia";
 
 export const useNotificacionStore = defineStore("notificacion", () => {
@@ -6,17 +6,20 @@ export const useNotificacionStore = defineStore("notificacion", () => {
     const texto = ref('');
     const error = ref(false);
 
-    function $reset() {
-        mostrar.value = false;
-        texto.value = '';
-        error.value = false;
-    }
+    watch(mostrar, () => {
+        if (mostrar.value) {
+            setTimeout(() => {
+                mostrar.value = false;
+                texto.value = '';
+                error.value = false;
+            }, 3000);
+        }
+    })
 
     return {
         mostrar,
         texto,
-        error,
-        $reset
+        error
     }
 
 });
