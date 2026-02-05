16be5c2724e5f05ab00449e0210912b31e388e25
adrianmendozatabaz <myamyamya29@gmail.com>
2026-02-05 16:35:28 -0600

    feat: end class 261 import ia store in IAView

--- src/views/IAView.vue
+++ src/views/IAView.vue
@@ -1,5 +1,7 @@
 <script setup>
+import { useIAStore } from '@/stores/ia';
 
+const store = useIAStore();
 </script>
 
 <template>
@@ -8,7 +10,8 @@
     <div class="max-w-4xl mx-auto">
         <form class='flex flex-col space-y-3 py-10'>
             <div class="relative">
-                <input name="prompt" id="prompt" class="border bg-white p-4 rounded-lg w-full border-slate-800"
+                <input name="prompt" id="prompt" v-model="store.prompt"
+                    class="border bg-white p-4 rounded-lg w-full border-slate-800"
                     placeholder="Genera una receta con ingredientes. Ej. Bebida con Tequila y Fresa" />
                 <button type="submit" aria-label="Enviar"
                     class="  cursor-pointer absolute top-1/2 right-5 transform -translate-x-1/2 -translate-y-1/2">
