cbb8809950d8726dcce9f0b9c664692f51f39128
adrianmendozatabaz <myamyamya29@gmail.com>
2026-02-05 16:51:05 -0600

    feat: end class 263 create generar receta function

---
+++ src/services/IAService.js
@@ -0,0 +1,12 @@
+import { openRouter } from "@/lib/ia";
+import { streamText } from "ai";
+
+export default {
+    async generarReceta(prompt) {
+        const result = streamText({
+            model: '',
+            prompt: ''
+        });
+
+    }
+}
