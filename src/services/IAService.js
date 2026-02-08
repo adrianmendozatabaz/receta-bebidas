e6d5d4b2554e843e1f235f5d835ea3614b28b5ff
adrianmendozatabaz <myamyamya29@gmail.com>
2026-02-08 11:59:24 -0600

    feat: end class 265 add models

--- src/services/IAService.js
+++ src/services/IAService.js
@@ -4,7 +4,9 @@ import { streamText } from "ai";
 export default {
     async generarReceta(prompt) {
         const result = streamText({
-            model: openRouter('meta-llama/llama-3.3-70b-instruct:free'),
+            // model: openRouter('meta-llama/llama-3.3-70b-instruct:free'),
+            // model: openRouter('stepfun/step-3.5-flash:free'),
+            model: openRouter('nvidia/nemotron-3-nano-30b-a3b:free'),
             prompt
         });
         return result.textStream;
