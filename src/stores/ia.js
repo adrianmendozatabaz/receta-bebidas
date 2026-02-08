89d93750bef54cb7dd969e982668e67f8e97bdca
adrianmendozatabaz <myamyamya29@gmail.com>
2026-02-08 12:12:57 -0600

    feat: end class 266 add cargando

--- src/stores/ia.js
+++ src/stores/ia.js
@@ -5,19 +5,24 @@ import { ref } from "vue";
 export const useIAStore = defineStore("ia", () => {
     const prompt = ref('');
     const result = ref('');
+    const cargando = ref(false);
 
     async function generarReceta() {
         result.value = '';
+        cargando.value = true;
         const resultado = await IAService.generarReceta(prompt.value);
 
         for await (const text of resultado) {
             result.value += text;
         }
+
+        cargando.value = false;
     }
 
     return {
         prompt,
         result,
+        cargando,
         generarReceta
     }
 })
